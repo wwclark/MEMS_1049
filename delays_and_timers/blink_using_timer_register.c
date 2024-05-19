81171fd8479cebfac94d04d7fe1d2218ef473374
William W Clark <wclark@pitt.edu>
2024-05-19 16:23:08 -0400

    Updatefor multiple clocks

--- delays_and_timers/blink_using_timer_register.c
+++ delays_and_timers/blink_using_timer_register.c
@@ -1,21 +1,23 @@
 /*
  * blink_using_timer_register.c
  *
- * Created: 5/17/2020
+ * Created: 5/19/2024
  * Author : WW Clark
  */
  
 /* 
  *
-  This program blinks an LED at set frequencies.  Connect LED to PORTC0.
+  This program blinks an LED at a defined frequency.  Connect LED to PORTC0.
   
-  Uses TIMER0 register at a single blink rate with a fixed timer prescaler.  To allow more general prescaler setting, see blink_with_timer_general_delay.c
+  Uses TIMER0 register at a single blink rate with a fixed timer prescaler.  
     
   Connect LED signal to oscilloscope to test frequencies.
 *
  */ 
 
 #include <avr/io.h>
+
+#define FREQ_CLK 1000000 // Clock frequency (default setting for the ATmega328P -- 8 MHz internal clock with divide-by-8
 
 //** Functions **//
 void wait(volatile int multiple);
@@ -32,26 +34,44 @@ int main(void)
    while(1)
     {
         // The main loop -- toggle the bit, then delay to set the frequency
-		PORTC = 0b00000001; // Turn off LED -- note that this also clears all other bits on PORTC
-		wait(250);
-		PORTC = 0b00000000; // Turn on LED --  note that this also clears all other bits on PORTC
-		wait(250);
-		
-		// An alternate and efficient way to toggle the LED on and off is shown below with single bit addressing.  This avoids affecting other bits on PORTC besides the one we want to address.
-        //PORTC = PORTC ^ 1<<PORTC0;  // Note that this uses the Exclusive OR operator, which toggles an individual bit without changing the other bits in PORTC
-        //wait(250); // call delay function: the wait function will loop 250 time using delay #2 (1 msec each loop) for a total of 250 msec delay     
-    }	
+        PORTC = PORTC ^ 1<<PORTC0;  // Note that this uses the Exclusive OR operator, which toggles an individual 
+			// bit without changing the other bits in PORTC
+                
+        wait(1000); // call delay function where the argument is number of msec to wait: the wait function will n times, each time creating a 1 msec delay
+         
+    }
+	
 	return(0);
 } // end main()
 
-void wait(volatile int N) {
-	// This subroutine creates a delay equal to N msec
-	while (N > 0) {
+void wait(volatile int number_of_msec) {
+	// This subroutine creates a delay equal to number_of_msec*T, where T is 1 msec
+	// It changes depending on the frequency defined by FREQ_CLK
+	char register_B_setting;
+	char count_limit;
+	
+	// Some typical clock frequencies: 
+	switch(FREQ_CLK) {
+		case 16000000:
+			register_B_setting = 0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1). 
+			count_limit = 250; // For prescaler of 64, a count of 250 will require 1 msec
+			break;
+		case 8000000:
+			register_B_setting =  0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1). 
+			count_limit = 125; // for prescaler of 64, a count of 125 will require 1 msec
+			break;
+		case 1000000:
+			register_B_setting = 0b00000010; // this will start the timer in Normal mode with prescaler of 8 (CS02 = 0, CS01 = 1, CS00 = 0). 
+			count_limit = 125; // for prescaler of 8, a count of 125 will require 1 msec
+			break;
+	}
+	
+	while (number_of_msec > 0) {
 		TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
 		TCNT0 = 0;  // preload value for testing on count = 250
-		TCCR0B =  0b00000011; //1<<CS01 | 1<<CS00;	TCCR0B = 0x03;  // Start TIMER0, Normal mode, 16 MHz crystal clock, prescaler = 64
-		while (TCNT0 < 0xFA); // exits when count = 250 (which is the count required to create a 1 msec delay with 16MHz clock and prescaler of 64)
+		TCCR0B =  register_B_setting;  // Start TIMER0 with the settings defined above
+		while (TCNT0 < count_limit); // exits when count = the required limit for a 1 msec delay
 		TCCR0B = 0x00; // Stop TIMER0
-		N--;
+		number_of_msec--;
 	}
 } // end wait()
