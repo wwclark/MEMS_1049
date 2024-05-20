db7da848a13f8153e3982a0ecae7ebfd05ca7cdd
William W Clark <wclark@pitt.edu>
2024-05-20 08:09:41 -0400

    Update wait function to accept multiple clock frequencies

--- Lab_1/main_special.c
+++ Lab_1/main_special.c
@@ -2,6 +2,7 @@
  * Lab1_Special.c
  *
  * Created: 1/16/2024 Based on Lab_1_2021
+ * Updated 5/20/2024 to update wait function
  * Author : WWClark
  */ 
 
@@ -17,6 +18,7 @@ This program is a solution to Lab 1 --
 
 #include <avr/io.h>
 
+#define FREQ_CLK 1000000
 
 //** Functions **//
 void wait(volatile int multiple);
@@ -161,14 +163,34 @@ int wait_for_switch_cycle(void) {
 }
 	
 	
-void wait(volatile int N) {
-	// This subroutine creates a delay equal to N msec
-	while (N > 0) {
+void wait(volatile int number_of_msec) {
+	// This subroutine creates a delay equal to number_of_msec*T, where T is 1 msec
+	// It changes depending on the frequency defined by FREQ_CLK
+	char register_B_setting;
+	char count_limit;
+	
+	// Some typical clock frequencies:
+	switch(FREQ_CLK) {
+		case 16000000:
+		register_B_setting = 0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1).
+		count_limit = 250; // For prescaler of 64, a count of 250 will require 1 msec
+		break;
+		case 8000000:
+		register_B_setting =  0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1).
+		count_limit = 125; // for prescaler of 64, a count of 125 will require 1 msec
+		break;
+		case 1000000:
+		register_B_setting = 0b00000010; // this will start the timer in Normal mode with prescaler of 8 (CS02 = 0, CS01 = 1, CS00 = 0).
+		count_limit = 125; // for prescaler of 8, a count of 125 will require 1 msec
+		break;
+	}
+
+	while (number_of_msec > 0) {
 		TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
 		TCNT0 = 0;  // preload value for testing on count = 250
-		TCCR0B =  0b00000011; //1<<CS01 | 1<<CS00;	TCCR0B = 0x03;  // Start TIMER0, Normal mode, 16 MHz crystal clock, prescaler = 64
-		while (TCNT0 < 0xFA); // exits when count = 250 (which is the count required to create a 1 msec delay with 16MHz clock and prescaler of 64)
+		TCCR0B =  register_B_setting;  // Start TIMER0 with the settings defined above
+		while (TCNT0 < count_limit); // exits when count = the required limit for a 1 msec delay
 		TCCR0B = 0x00; // Stop TIMER0
-		N--;
+		number_of_msec--;
 	}
 } // end wait()
