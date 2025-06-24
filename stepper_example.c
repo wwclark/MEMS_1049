50f16bd54012c7aceb017f82bfba8fa7ac9f2753
William W Clark <wclark@pitt.edu>
2025-06-24 13:25:20 -0400

    Update stepper_example.c
    Update wait function to use a single argument.

--- stepper_example.c
+++ stepper_example.c
@@ -10,11 +10,12 @@
 
 #include <avr/io.h>
 
+#define FREQ_CLK 1000000 // Clock frequency (default setting for the ATmega328P -- 8 MHz internal clock with divide-by-8
+
 // Functions
 void step_CW( void );
 void step_CCW( void );
-void wait(volatile int multiple, volatile char time_choice);
-void delay_T_msec_timer0(char choice);
+void wait(volatile int multiple);
 
 //** GLOBAL VARIABLES **
 int phase_step = 1; // We will use wave stepping, so the steps are numbered 1-4, then will repeat.  
@@ -27,13 +28,13 @@ int main(void)
    {
 	// Step forward two steps
 	step_CW(); //step_CW()
-	wait(250,2); // delay so that motor doesn't skip steps
+	wait(250); // delay so that motor doesn't skip steps
 	step_CW(); //step_CW()
-	wait(250,2); // delay so that motor doesn't skip steps
+	wait(250); // delay so that motor doesn't skip steps
 
 	// Step backward one step
 	step_CCW(); // step_CCW()
-	wait(250,2); // delay so that motor doesn't skip steps
+	wait(250); // delay so that motor doesn't skip steps
   
    } // end main while
 
@@ -93,61 +94,34 @@ void step_CW() {
 	break;  }
 }// end step_CW
 
-void wait(volatile int multiple, volatile char time_choice) {
-	/* This subroutine calls others to create a delay.
-		 Total delay = multiple*T, where T is in msec and is the delay created by the called function.
+void wait(volatile int number_of_msec) {
+	// This subroutine creates a delay equal to number_of_msec*T, where T is 1 msec
+	// It changes depending on the frequency defined by FREQ_CLK
+	char register_B_setting;
+	char count_limit;
 	
-		Inputs: multiple = number of multiples to delay, where multiple is the number of times an actual delay loop is called.
-		Outputs: None
-	*/
+	// Some typical clock frequencies:
+	switch(FREQ_CLK) {
+		case 16000000:
+		register_B_setting = 0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1).
+		count_limit = 250; // For prescaler of 64, a count of 250 will require 1 msec
+		break;
+		case 8000000:
+		register_B_setting =  0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1).
+		count_limit = 125; // for prescaler of 64, a count of 125 will require 1 msec
+		break;
+		case 1000000:
+		register_B_setting = 0b00000010; // this will start the timer in Normal mode with prescaler of 8 (CS02 = 0, CS01 = 1, CS00 = 0).
+		count_limit = 125; // for prescaler of 8, a count of 125 will require 1 msec
+		break;
+	}
 	
-	while (multiple > 0) {
-		delay_T_msec_timer0(time_choice); 
-		multiple--;
+	while (number_of_msec > 0) {
+		TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
+		TCNT0 = 0;  // preload value for testing on count = 250
+		TCCR0B =  register_B_setting;  // Start TIMER0 with the settings defined above
+		while (TCNT0 < count_limit); // exits when count = the required limit for a 1 msec delay
+		TCCR0B = 0x00; // Stop TIMER0
+		number_of_msec--;
 	}
 } // end wait()
-
-void delay_T_msec_timer0(volatile char choice) {
-    //*** delay T ms **
-    /* This subroutine creates a delay of T msec using TIMER0 with prescaler on clock, where, for a 16MHz clock:
-    		for Choice = 1: T = 0.125 msec for prescaler set to 8 and count of 250 (preload counter with 5)
-    		for Choice = 2: T = 1 msec for prescaler set to 64 and count of 250 (preload counter with 5)
-    		for Choice = 3: T = 4 msec for prescaler set to 256 and count of 250 (preload counter with 5)
-    		for Choice = 4: T = 16 msec for prescaler set to 1,024 and count of 250 (preload counter with 5)
-			for Choice = Default: T = .0156 msec for no prescaler and count of 250 (preload counter with 5)
-	
-			Inputs: None
-			Outputs: None
-	*/
-	
-	TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
-	TCNT0 = 0;  // preload value for testing on count = 250
-	// preload value for alternate test on while loop: TCNT0 = 5;  // preload load TIMER0  (count must reach 255-5 = 250)
-	
-	switch ( choice ) { // choose prescaler
-		case 1:
-			TCCR0B = 0b00000010; //1<<CS01;	TCCR0B = 0x02; // Start TIMER0, Normal mode, crystal clock, prescaler = 8
-		break;
-		case 2:
-			TCCR0B =  0b00000011; //1<<CS01 | 1<<CS00;	TCCR0B = 0x03;  // Start TIMER0, Normal mode, crystal clock, prescaler = 64
-		break;
-		case 3:
-			TCCR0B = 0b00000100; //1<<CS02;	TCCR0B = 0x04; // Start TIMER0, Normal mode, crystal clock, prescaler = 256
-		break; 
-		case 4:
-			TCCR0B = 0b00000101; //1<<CS02 | 1<<CS00; TCCR0B = 0x05; // Start TIMER0, Normal mode, crystal clock, prescaler = 1024
-		break;
-		default:
-			TCCR0B = 0b00000001; //1<<CS00; TCCR0B = 0x01; Start TIMER0, Normal mode, crystal clock, no prescaler
-		break;
-	}
-	
-	while (TCNT0 < 0xFA); // exits when count = 250 (requires preload of 0 to make count = 250)
-	// alternate test on while loop: while ((TIFR0 & (0x1<<TOV0)) == 0); // wait for TOV0 to roll over:
-	// How does this while loop work?? See notes
-	
-	TCCR0B = 0x00; // Stop TIMER0
-	//TIFR0 = 0x1<<TOV0;  // Alternate while loop: Clear TOV0 (note that this is a nonintuitive bit in that it is cleared by writing a 1 to it)
-	
-} // end delay_T_msec_timer0()
-
